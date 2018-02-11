0e34eb31bbf047cd96de1f41736eef4bfdc09a8f
Han Na <hosseni@hotmail.com>
2018-02-11 16:30:25 -0600

    demo03 fully connected network, added node, layer, connection.

---
+++ demo03_fully_connected_network.py
@@ -0,0 +1,123 @@
+import math
+import random
+
+
+def sigmoid(x):
+    return 1.0 / (1 + math.exp(-x))
+
+
+class Node(object):
+    def __init__(self, layer_index, node_index):
+        self.layer_index = layer_index
+        self.node_index = node_index
+        self.upstream = []
+        self.downstream = []
+        self.output = 0
+        self.delta = 0
+
+    def set_output(self, output):
+        self.output = output
+
+    def append_upstream_connection(self, conn):
+        self.upstream.append(conn)
+
+    def append_downstream_connection(self, conn):
+        self.downstream.append(conn)
+
+    def calc_output(self):
+        output = reduce(lambda ret, conn: ret + conn.upstream_node.output * conn.weight, self.upstream, 0)
+        self.output = sigmoid(output)
+
+    def calc_hidden_layer_delta(self):
+        downstream_delta = reduce(lambda ret, conn: ret + conn.downstream_node.delta * conn.weight, self.downstream, 0.0)
+        self.delta = self.output * (1 - self.output) * downstream_delta
+
+    def calc_output_layer_delta(self, label):
+        self.delta = self.output * (1 - self.output) * (label - self.output)
+
+    def __str__(self):
+        node_str = '%u-%u: output: %f delta: %f' % (self.layer_index, self.node_index, self.output, self.delta)
+        downstream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn), self.downstream, ' ')
+        upstream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn), self.upstream, ' ')
+        return node_str + '\n\tdownstream: ' + downstream_str + '\n\tupstream: ' + upstream_str
+
+
+class ConstNode(object):
+    def __init__(self, layer_index, node_index):
+        self.layer_index = layer_index
+        self.node_index = node_index
+        self.downstream = []
+        self.output = 1
+        self.delta = 0
+
+    def append_downstream_connection(self, conn):
+        self.downstream.append(conn)
+
+    def calc_hidden_layer_delta(self):
+        downstream_delta = reduce(lambda ret, conn: ret + conn.downstream_node.delta * conn.weight, self.downstream, 0.0)
+        self.delta = self.output * (1 - self.output) * downstream_delta
+
+    def __str__(self):
+        node_str = '%u-%u: output: %f delta: %f' % (self.layer_index, self.node_index, self.output, self.delta)
+        downstream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn), self.downstream, ' ')
+        return node_str + '\n\tdownstream: ' + downstream_str
+
+
+class Layer(object):
+    def __init__(self, layer_index, node_count):
+        self.layer_index = layer_index
+        self.nodes = []
+        for i in xrange(node_count):
+            self.nodes.append(Node(layer_index, i))
+        self.nodes.append(ConstNode(layer_index, node_count))
+
+    def set_output(self, data):
+        for i in xrange(len(data)):
+            self.nodes[i].set_output(data[i])
+
+    def calc_output(self):
+        for node in self.nodes[:-1]:
+            node.calc_output()
+
+    def dump(self):
+        for node in self.nodes:
+            print(node)
+
+class Connection(object):
+    def __init__(self, upstream_node, downstream_node):
+        self.upstream_node = upstream_node
+        self.downstream_node = downstream_node
+        self.weight = random.uniform(-0.1, 0.1)
+        self.gradient = 0.0
+
+    def calc_gradient(self):
+        self.gradient = self.downstream_node.delta * self.upstream_node.output
+
+    def get_gradient(self):
+        return self.gradient
+
+    def update_weight(self, rate):
+        self.calc_gradient()
+        self.weight += rate * self.gradient
+
+    def __str__(self):
+        return '(%u-%u) -> (%u-%u) = %f' % (
+            self.upstream_node.layer_index,
+            self.upstream_node.node_index,
+            self.downstream_node.layer_index,
+            self.downstream_node.node_index,
+            self.weight)
+
+class Connections(object):
+    def __init__(self):
+        self.connections = []
+
+    def add_connection(self, connection):
+        self.connections.append(connection)
+
+    def dump(self):
+        for conn in self.connections:
+            print(conn)
+
+
+
